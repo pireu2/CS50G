2086191b73fe72c7688bd8cfb8d7ee89a4f69ab9
Duica Sebastian <sebi.duica@gmail.com>
2024-02-24 23:11:08 +0200

    project 9 done

---
+++ Project 9/dreadhalls/Assets/Scripts/MazeCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeCount : MonoBehaviour
+{
+	private static int mazeCount = 0;
+	private Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+		text = gameObject.GetComponent<Text>();
+	}
+
+	public static void Initialize()
+	{
+		mazeCount = 0;
+	}
+	public static void Increment()
+	{
+		mazeCount++;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = "Mazes: " + mazeCount;
+	}
+}
