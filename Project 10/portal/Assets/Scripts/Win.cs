60b52c64233a2e722c0247dd059a5043e2b6d9c2
Duica Sebastian <sebi.duica@gmail.com>
2024-02-26 23:36:47 +0200

    project 10 done

---
+++ Project 10/portal/Assets/Scripts/Win.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour {
+    public Text winText;
+
+	// Use this for initialization
+	void Start()
+	{
+		winText.color = Color.clear;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (winText.color == Color.black && Input.GetKeyDown(KeyCode.Space))
+		{
+			SceneManager.LoadScene("Play");
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		winText.color = Color.black;
+	}
+}
